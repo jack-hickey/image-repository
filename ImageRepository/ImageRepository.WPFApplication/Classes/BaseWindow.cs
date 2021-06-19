25a13435f5e9524db8a55e8ecbc752de3a112d30
Jack Hickey <jack.hickey@outlook.com>
2021-06-19 18:03:56 +0100

    Created BaseWindow class

---
+++ ImageRepository/ImageRepository.WPFApplication/Classes/BaseWindow.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Media;
+using System.Windows.Shell;
+
+namespace ImageRepository.WPFApplication.Classes
+{
+    public class BaseWindow : Window
+    {
+        public BaseWindow()
+        {
+            try
+            {
+                WindowChrome.SetWindowChrome(this, new WindowChrome()
+                {
+                    CornerRadius = SystemParameters.WindowCornerRadius,
+                    CaptionHeight = SystemParameters.CaptionHeight,
+                    GlassFrameThickness = SystemParameters.WindowResizeBorderThickness
+                });
+
+                TextElement.SetForeground(this, Application.Current.Resources["MaterialDesignBody"] as Brush);
+                TextElement.SetFontWeight(this, FontWeights.Regular);
+                TextElement.SetFontSize(this, 13);
+
+                TextOptions.SetTextFormattingMode(this, TextFormattingMode.Ideal);
+                TextOptions.SetTextRenderingMode(this, TextRenderingMode.Auto);
+
+                this.Background = Application.Current.Resources["MaterialDesignPaper"] as Brush;
+                this.FontFamily = Application.Current.Resources["MaterialDesignFont"] as FontFamily;
+
+                this.WindowStyle = WindowStyle.None;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"An error occurred creating instance: {ex}", ex);
+            }
+        }
+    }
+}
--- ImageRepository/ImageRepository.WPFApplication/Windows/LandingPage.xaml.cs
+++ ImageRepository/ImageRepository.WPFApplication/Windows/LandingPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ImageRepository.WPFApplication.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,7 +18,7 @@ namespace ImageRepository.WPFApplication.Windows
     /// <summary>
     /// Interaction logic for LandingPage.xaml
     /// </summary>
-    public partial class LandingPage : Window
+    public partial class LandingPage : BaseWindow
     {
         public LandingPage()
         {
