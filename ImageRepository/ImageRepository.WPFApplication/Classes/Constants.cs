f468b528a07a7c6d0826867526714a2bf027f6ce
Jack Hickey <jack.hickey@outlook.com>
2021-06-19 20:42:12 +0100

    Now able to open a file dialog that filters by image files

---
+++ ImageRepository/ImageRepository.WPFApplication/Classes/Constants.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageRepository.WPFApplication.Classes
+{
+    public static class Constants
+    {
+        public static OpenFileDialog ImagePrompt = new OpenFileDialog()
+        {
+            Multiselect = true,
+            Filter = "Images (*.jpg;*.jpeg;*.png;*.gif)|*.jpg;*.jpeg;*.png;*.gif"
+        };
+    }
+}
--- ImageRepository/ImageRepository.WPFApplication/Windows/LandingPage.xaml.cs
+++ ImageRepository/ImageRepository.WPFApplication/Windows/LandingPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using ImageRepository.WPFApplication.Classes;
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,6 +24,21 @@ namespace ImageRepository.WPFApplication.Windows
         public LandingPage()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (Constants.ImagePrompt.ShowDialog() ?? false)
+                {
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Globals.HandleError(ex);
+            }
         }
     }
 }
