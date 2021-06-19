d69a6de80158b7c3c75c8506bbd4509ea65c8a55
Jack Hickey <jack.hickey@outlook.com>
2021-06-19 17:47:31 +0100

    Created LandingPage window

--- ImageRepository/ImageRepository.WPFApplication/Windows/LandingPage.xaml.cs
+++ ImageRepository/ImageRepository.WPFApplication/Windows/LandingPage.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ImageRepository.WPFApplication
+namespace ImageRepository.WPFApplication.Windows
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for LandingPage.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class LandingPage : Window
     {
-        public MainWindow()
+        public LandingPage()
         {
             InitializeComponent();
         }
